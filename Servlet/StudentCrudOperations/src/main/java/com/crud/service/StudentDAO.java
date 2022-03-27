2e3a4ee91ccf7f9f5637da0e28fc49883014506b
niitagile <43024343+niitagile@users.noreply.github.com>
2022-03-27 20:44:55 +0530

    Add files via upload

--- Servlet/StudentCrudOperations/src/main/java/com/crud/service/StudentDAO.java
+++ Servlet/StudentCrudOperations/src/main/java/com/crud/service/StudentDAO.java
@@ -1,10 +1,14 @@
 package com.crud.service;
 
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.sql.Statement;
 import com.crud.model.Student;
+
 
 public class StudentDAO {
 	private static Connection getConnection() {
@@ -37,10 +41,36 @@ public class StudentDAO {
 			ps.setString(2,student.getStudname());
 			ps.setDate(3, sdob);
 			result=ps.executeUpdate();
+			con.close();
 			}
 			catch(Exception e) {
 				e.printStackTrace();
 			}
 			return result;
+		}
+		
+		public ArrayList<Student> getRecords(){
+			ArrayList<Student> list=new ArrayList<>();
+			try {
+				Connection con=getConnection();
+				//To get all Records
+				Statement stmt=con.createStatement();
+				ResultSet rs=stmt.executeQuery("select * from studentdetails");
+				//Add all Records in ArrayList		
+								
+				while(rs.next()) {
+					
+					list.add(new Student(rs.getInt(1),rs.getString(2),rs.getDate(3)));
+					
+					
+					
+				}
+				stmt.close();
+				con.close();
+			}
+				catch(Exception e) {
+				e.printStackTrace();
+			}
+			return list;
 		}
 }
